e25a338f32a3ad3610c3cde54e63b5e6a6ba1302
Aaron Ruettinger <aaron.ruettinger@gmail.com>
2015-11-19 12:24:14 -0500

    User profile completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root "welcome#index"
+  #unauthenticated - signed out users
+  #authenticated - signed in users
+  unauthenticated do
+    # get "/", to: "welcome#index", as: :root
+    root "welcome#index"
+  end
+
+  authenticated do
+    # get "/", to: "users#show", as: :authenticated_root
+    root "users#show", as: :authenticated_root
+  end
 end
